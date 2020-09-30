a40f1da03a66001fae1d5e2707c3bd9c13cfa4b3
yokomaruha <72114763+yuuya-yokoyama@users.noreply.github.com>
2020-09-30 16:52:11 +0900

    Update EchoApplication.java

--- src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -41,7 +41,7 @@ public class EchoApplication {
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         log.info("event: " + event);
-        final String originalMessageText = event.getMessage().getText();
+        final String originalMessageText = event.getMessage().getText() + "1234";
         return new TextMessage(originalMessageText + " abcde");
     }
 
